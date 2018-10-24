23cd886a3618735c12a9dab0dee037a25703f76b
Michael <mspitsin@luxoft.com>
2018-10-24 09:34:29 +0300

    Init commit

---
+++ app/src/main/java/com/gihub/programmerr47/fragmentnavigationsandbox/SandboxActivity.kt
@@ -0,0 +1,12 @@
+package com.gihub.programmerr47.fragmentnavigationsandbox
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SandboxActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sandbox)
+    }
+}
